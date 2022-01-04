f7f3c0a8494f3f0627c344302d110387c295c6c1
slodkae <elena.zapolny@epitech.eu>
2022-01-04 17:50:35 +0100

    final project

---
+++ dictionnaire.py
@@ -0,0 +1,8 @@
+nouvelle_campagne = {
+    "responsable_de_campagne": "Jeanne d'Arc",
+    "nom_de_campagne": "Campagne nous aimons les chiens",
+    "date_de_debut": "01/01/2021",
+    "influenceurs_importants": ["@MonAmourDeChien", "@MeilleuresFriandisesPourChiens"]
+}
+
+print(nouvelle_campagne)
